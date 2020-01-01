06fc1ea89b78bf171804310f70f43d6873a70e86
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2020-01-01 16:29:26 -0500

    Add file copyright headers.

--- src/main/java/org/eclipse/dash/licenses/InvalidContentId.java
+++ src/main/java/org/eclipse/dash/licenses/InvalidContentId.java
@@ -1,3 +1,12 @@
+/*************************************************************************
+ * Copyright (c) 2019, The Eclipse Foundation and others.
+ * 
+ * This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License 2.0 which accompanies this 
+ * distribution, and is available at https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *************************************************************************/
 package org.eclipse.dash.licenses;
 
 public class InvalidContentId implements IContentId {
