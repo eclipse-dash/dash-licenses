462d40b50244b8c5dc43f9d9a35d6b03d8823516
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2020-12-10 18:56:30 -0500

    Include more information in the review text.
    Improve the quality of the IPZilla search. Add a link to the source on
    Maven Central.

--- src/main/java/org/eclipse/dash/licenses/cli/CreateReviewRequestCollector.java
+++ src/main/java/org/eclipse/dash/licenses/cli/CreateReviewRequestCollector.java
@@ -11,9 +11,13 @@ package org.eclipse.dash.licenses.cli;
 
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.dash.licenses.IContentData;
 import org.eclipse.dash.licenses.IContentId;
@@ -66,7 +70,7 @@ public class CreateReviewRequestCollector implements IResultsCollector {
 	private void describe(LicenseData licenseData) {
 		output.println(String.format("* [ ] %s", licenseData.getId()));
 		licenseData.contentData().forEach(data -> describeItem(data));
-		String searchUrl = getIPZillaSearchUrl(licenseData);
+		String searchUrl = IPZillaSearchBuilder.build(licenseData);
 		if (searchUrl != null) {
 			output.println(String.format("  - [Search IPZilla](%s)", searchUrl));
 		}
@@ -74,6 +78,10 @@ public class CreateReviewRequestCollector implements IResultsCollector {
 		if ("maven".equals(id.getType()) && "mavencentral".equals(id.getSource())) {
 			output.println(String.format("  - [Maven Central](https://search.maven.org/artifact/%s/%s/%s/jar)",
 					id.getNamespace(), id.getName(), id.getVersion()));
+			var source = getMavenSourceUrl(id);
+			if (source != null) {
+				output.println(String.format("  - [Source](%s) from Maven Central", source));
+			}
 		}
 		if ("npm".equals(id.getType()) && "npmjs".equals(id.getSource())) {
 			var builder = new StringBuilder();
@@ -107,40 +115,93 @@ public class CreateReviewRequestCollector implements IResultsCollector {
 		};
 	}
 
-	private String getIPZillaSearchUrl(LicenseData licenseData) {
-		if (!licenseData.getId().isValid())
-			return null;
-
-		var terms = new HashSet<String>();
-
-		String namespace = licenseData.getId().getNamespace();
-		String name = licenseData.getId().getName();
-
-		// Assemble terms from the content data that might result
-		// in an interesting search.
-		terms.add(namespace);
-		terms.add(name);
-
-		// Add the last segment from the name to the search terms.
-		// We arbitrarily decide that terms that are "short" aren't interesting
-		// and skip them. The logic being that shorter words are more likely to
-		// be common, and common words will clutter up our search.
-		String lastSegment = name.substring(1 + name.lastIndexOf('.'));
-		if (lastSegment.length() >= 8)
-			terms.add(lastSegment);
-
-		var builder = new StringBuilder();
-		builder.append("https://dev.eclipse.org/ipzilla/buglist.cgi");
-		builder.append("?short_desc_type=anywords");
-		builder.append("&short_desc=");
-		builder.append(String.join("+", terms));
-		builder.append("&long_desc_type=substring");
-
-		return builder.toString();
-	}
-
 	@Override
 	public int getStatus() {
 		return needsReview.size();
+	}
+
+	private String getMavenSourceUrl(IContentId id) {
+		if (!id.isValid())
+			return null;
+
+		// FIXME Validate that this file pattern is correct.
+		// This pattern was observed and appears to be accurate.
+		var url = "https://search.maven.org/remotecontent?filepath={groupPath}/{artifactid}/{version}/{artifactid}-{version}-sources.jar";
+		url = url.replace("{groupPath}", id.getNamespace().replace('.', '/'));
+		url = url.replace("{artifactid}", id.getName());
+		url = url.replace("{version}", id.getVersion());
+
+		if (remoteFileExists(url)) {
+			return url;
+		}
+
+		return null;
+	}
+
+	private static boolean remoteFileExists(String url) {
+		try {
+			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setRequestMethod("HEAD");
+			return (connection.getResponseCode() == HttpURLConnection.HTTP_OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	static final class IPZillaSearchBuilder {
+		private static final String[] COMMON_TERMS = new String[] { "apache", "eclipse", "source", "platform", "plugin",
+				"parent", "client", "server" };
+		private static final int SEARCH_TERM_MINIMUM_LENGTH = 5;
+
+		Set<String> terms = new LinkedHashSet<>();
+
+		public static String build(LicenseData licenseData) {
+			return new IPZillaSearchBuilder().get(licenseData);
+		}
+
+		private void add(String term) {
+			// Break the name into segments (non-word characters) and add the segments from
+			// the name to the search terms. We arbitrarily decide that terms that are
+			// "short" aren't interesting and skip them. The logic being that shorter words
+			// are more likely to be common, and common words will clutter up our search.
+			if (Arrays.stream(COMMON_TERMS).anyMatch(each -> each.equalsIgnoreCase(term)))
+				return;
+			if (term.length() >= SEARCH_TERM_MINIMUM_LENGTH)
+				terms.add(term);
+		}
+
+		private String get(LicenseData licenseData) {
+			if (!licenseData.getId().isValid())
+				return null;
+
+			String namespace = licenseData.getId().getNamespace();
+			String name = licenseData.getId().getName();
+
+			// Assemble terms from the content data that might result
+			// in an interesting search.
+			terms.add(namespace);
+			terms.add(name);
+
+			// Break the name into segments (non-word characters) and add the segments from
+			// the name to the search terms.
+			for (String segment : name.split("\\W"))
+				add(segment);
+
+			for (String segment : namespace.split("\\W"))
+				add(segment);
+
+			if (terms.isEmpty())
+				return null;
+
+			var builder = new StringBuilder();
+			builder.append("https://dev.eclipse.org/ipzilla/buglist.cgi");
+			builder.append("?short_desc_type=anywords");
+			builder.append("&short_desc=");
+			builder.append(String.join("+", terms));
+			builder.append("&long_desc_type=substring");
+
+			return builder.toString();
+		}
 	}
 }
