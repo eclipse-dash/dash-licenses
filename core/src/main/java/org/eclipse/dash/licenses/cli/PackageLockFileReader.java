448b95bd9a025ef5dea069b4cc2bdaf4ed0483f1
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2021-04-17 00:24:03 -0400

    Support package-lock.json v2.

--- core/src/main/java/org/eclipse/dash/licenses/cli/PackageLockFileReader.java
+++ core/src/main/java/org/eclipse/dash/licenses/cli/PackageLockFileReader.java
@@ -1,10 +1,20 @@
+/*************************************************************************
+ * Copyright (c) 2020,2021 The Eclipse Foundation and others.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution, and is available at https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *************************************************************************/
 package org.eclipse.dash.licenses.cli;
 
 import java.io.InputStream;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.dash.licenses.ContentId;
 import org.eclipse.dash.licenses.IContentId;
@@ -15,49 +25,110 @@ import jakarta.json.JsonObject;
 
 public class PackageLockFileReader implements IDependencyListReader {
 
-	private InputStream input;
+	private final InputStream input;
 
 	public PackageLockFileReader(InputStream input) {
 		this.input = input;
 	}
 
-	@Override
-	public Collection<IContentId> getContentIds() {
-		Set<IContentId> content = new HashSet<>();
-		JsonObject json = JsonUtils.readJson(input);
-		dependenciesDo(json.getJsonObject("dependencies"), id -> content.add(id));
-		return content;
+	/**
+	 * Version 2 of the package-lock.json format includes a flat associative array
+	 * of "packages"; each entry contains information about one of the dependencies.
+	 * Each instance of this class represents one package entry.
+	 */
+	class Package {
+		final String key;
+		final JsonObject value;
+
+		Package(String key, JsonObject value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		/**
+		 * The content id needs to be extracted from a combination of the key and values
+		 * from the associated associative array.
+		 * <p>
+		 * For version 1, the key contains the name, and--when one is defined--the
+		 * scope:
+		 * <ul>
+		 * <li>junk</li>
+		 * <li>@babel/junk</li>
+		 * </ul>
+		 * 
+		 * <p>
+		 * For version 2, the key also includes the path:
+		 * 
+		 * <ul>
+		 * <li>node_modules/@types/babel__core</li>
+		 * <li>node_modules/@types/babel__generator</li>
+		 * <li>node_modules/@jest/transform/node_modules/slash</li>
+		 * <li>node_modules/acorn-globals</li>
+		 * </ul>
+		 * 
+		 * <p>
+		 * It's relatively easy to handle all cases with a regular expression.
+		 */
+		IContentId getContentId() {
+			Pattern pattern = Pattern.compile("(?:(?<scope>@[^\\/]+)\\/)?(?<name>[^\\/]+)$");
+			Matcher matcher = pattern.matcher(key);
+			if (matcher.find()) {
+				var namespace = matcher.group("scope");
+				if (namespace == null)
+					namespace = "-";
+				var name = matcher.group("name");
+				var version = value.asJsonObject().getString("version");
+
+				return ContentId.getContentId("npm", "npmjs", namespace, name, version);
+			}
+			return new InvalidContentId(key);
+		}
 	}
 
 	/**
-	 * Walk through the structure to identify the dependencies, and (recursively)
-	 * the dependencies of the dependencies.
-	 *
-	 * @param json     A "dependency" list from a package-lock.json file
-	 * @param consumer A single argument consumer of each listed item.
+	 * Version 1 of the package-lock.json format has a notion of dependencies. Each
+	 * dependency record contains information about a single resolve dependency.
+	 * Every dependency can have its own dependencies (recursive).
 	 */
-	private void dependenciesDo(JsonObject json, Consumer<IContentId> consumer) {
-		if (json == null)
-			return;
-		json.forEach((key, value) -> {
-			String[] parts = key.split("/");
-			String namespace;
-			String name;
-			if (parts.length == 2) {
-				namespace = parts[0];
-				name = parts[1];
-			} else {
-				namespace = "-";
-				name = key;
-			}
-			String version = value.asJsonObject().getString("version");
-			IContentId contentId = ContentId.getContentId("npm", "npmjs", namespace, name, version);
-			if (contentId == null)
-				contentId = new InvalidContentId("" + namespace + "/" + name + "@" + version);
-			consumer.accept(contentId);
+	class Dependency extends Package {
 
-			dependenciesDo(json.getJsonObject("dependencies"), consumer);
-		});
+		Dependency(String key, JsonObject value) {
+			super(key, value);
+		}
+
+		Stream<Dependency> getDependencies() {
+			JsonObject dependencies = value.getJsonObject("dependencies");
+			if (dependencies == null)
+				return Stream.empty();
+
+			return dependencies.entrySet().stream()
+					.map(each -> new Dependency(each.getKey(), each.getValue().asJsonObject()));
+		}
+
+		/**
+		 * Flatten the arbitrary hierarchy (tree) of dependencies into a single stream.
+		 */
+		Stream<Dependency> stream() {
+			return Stream.concat(Stream.of(this), getDependencies().flatMap(Dependency::stream));
+		}
+	}
+
+	@Override
+	public Collection<IContentId> getContentIds() {
+		JsonObject json = JsonUtils.readJson(input);
+
+		switch (json.getJsonNumber("lockfileVersion").intValue()) {
+		case 1:
+			return new Dependency("", json).stream().filter(each -> !each.key.isEmpty())
+					.map(dependency -> dependency.getContentId()).collect(Collectors.toList());
+		case 2:
+		case 3:
+			return json.getJsonObject("packages").entrySet().stream().filter(entry -> !entry.getKey().isEmpty())
+					.map(entry -> new Package(entry.getKey(), entry.getValue().asJsonObject()))
+					.map(dependency -> dependency.getContentId()).distinct().collect(Collectors.toList());
+		}
+
+		return null;
 	}
 
 }
--- core/src/test/java/org/eclipse/dash/licenses/tests/PackageLockFileReaderTests.java
+++ core/src/test/java/org/eclipse/dash/licenses/tests/PackageLockFileReaderTests.java
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright (c) 2020, The Eclipse Foundation and others.
+ * Copyright (c) 2020,2021 The Eclipse Foundation and others.
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -21,12 +21,23 @@ import org.junit.jupiter.api.Test;
 class PackageLockFileReaderTests {
 
 	@Test
-	void testMultipleBatches() throws IOException {
+	void testV1Format() throws IOException {
 		try (InputStream input = this.getClass().getResourceAsStream("/package-lock.json")) {
 			PackageLockFileReader reader = new PackageLockFileReader(input);
 			String[] expected = { "npm/npmjs/-/loglevel/1.6.1", "npm/npmjs/-/sax/1.2.4", "npm/npmjs/-/saxes/3.1.9",
 					"npm/npmjs/-/slimdom-sax-parser/1.1.3", "npm/npmjs/-/slimdom/2.2.1", "npm/npmjs/-/xml-js/1.6.11",
 					"npm/npmjs/-/xmlchars/1.3.1", "npm/npmjs/@namespace/fontoxpath/3.3.0" };
+			String[] found = reader.getContentIds().stream().map(IContentId::toString).sorted().toArray(String[]::new);
+			assertArrayEquals(expected, found);
+		}
+	}
+
+	@Test
+	void testV2Format() throws IOException {
+		try (InputStream input = this.getClass().getResourceAsStream("/package-lock-v2.json")) {
+			PackageLockFileReader reader = new PackageLockFileReader(input);
+			String[] expected = { "npm/npmjs/-/delayed-stream/1.0.0", "npm/npmjs/-/is-descriptor/1.0.2",
+					"npm/npmjs/@babel/code-frame/7.12.13", };
 			String[] found = reader.getContentIds().stream().map(IContentId::toString).sorted().toArray(String[]::new);
 			assertArrayEquals(expected, found);
 		}
