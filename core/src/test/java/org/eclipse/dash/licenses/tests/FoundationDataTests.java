ff50a7c04d1349660b2e3de829068eace0a246f2
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2021-07-08 22:03:50 -0400

    Improve testing coverage.

--- core/src/test/java/org/eclipse/dash/licenses/tests/FoundationDataTests.java
+++ core/src/test/java/org/eclipse/dash/licenses/tests/FoundationDataTests.java
@@ -16,15 +16,11 @@ import java.io.StringReader;
 import org.eclipse.dash.licenses.LicenseSupport.Status;
 import org.eclipse.dash.licenses.foundation.FoundationData;
 import org.eclipse.dash.licenses.util.JsonUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FoundationDataTests {
-
-	private FoundationData data;
-
-	@BeforeEach
-	void setup() {
+	@Test
+	void testStringConfidence() {
 		// @formatter:off
 		String json =
 			"{\n" +
@@ -38,16 +34,51 @@ class FoundationDataTests {
 			"}\n";
 		// @formatter:on
 
-		data = new FoundationData(JsonUtils.readJson(new StringReader(json)));
-	}
-
-	@Test
-	void testGetUrl() {
+		FoundationData data = new FoundationData(JsonUtils.readJson(new StringReader(json)));
 		assertEquals("https://dev.eclipse.org/ipzilla/show_bug.cgi?id=7766", data.getUrl());
+		assertEquals(Status.Approved, data.getStatus());
+		assertEquals(100, data.getScore());
 	}
 
 	@Test
-	void testGetStatus() {
+	void testNumericConfidence() {
+		// @formatter:off
+		String json =
+			"{\n" +
+			"	\"authority\": \"CQ7766\", \n" +
+			"	\"confidence\": 100, \n" +
+			"	\"definitionUrl\": null, \n" +
+			"	\"id\": \"maven/mavencentral/com.google.guava/guava/15.0\", \n" +
+			"	\"license\": \"Apache-2.0\", \n" +
+			"	\"sourceUrl\": null, \n" +
+			"	\"status\": \"approved\"\n" +
+			"}\n";
+		// @formatter:on
+
+		FoundationData data = new FoundationData(JsonUtils.readJson(new StringReader(json)));
+		assertEquals("https://dev.eclipse.org/ipzilla/show_bug.cgi?id=7766", data.getUrl());
 		assertEquals(Status.Approved, data.getStatus());
+		assertEquals(100, data.getScore());
+	}
+
+	@Test
+	void testIPLabAuthority() {
+		// @formatter:off
+		String json =
+			"{\n" +
+			"	\"authority\": \"#78\", \n" +
+			"	\"confidence\": 100, \n" +
+			"	\"definitionUrl\": null, \n" +
+			"	\"id\": \"maven/mavencentral/com.google.guava/guava/15.0\", \n" +
+			"	\"license\": \"Apache-2.0\", \n" +
+			"	\"sourceUrl\": null, \n" +
+			"	\"status\": \"approved\"\n" +
+			"}\n";
+		// @formatter:on
+
+		FoundationData data = new FoundationData(JsonUtils.readJson(new StringReader(json)));
+		assertEquals("https://gitlab.eclipse.org/eclipsefdn/emo-team/iplab/-/issues/78", data.getUrl());
+		assertEquals(Status.Approved, data.getStatus());
+		assertEquals(100, data.getScore());
 	}
 }
